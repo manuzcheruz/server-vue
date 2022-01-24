5eac96fcd2d6be0b77990d310239f37cd43ae33d
Kipkemoi Emmanuel <kipkemoi@Kipkemois-MacBook-Air.local>
2022-01-24 10:06:15 +0300

    added update and delete functionality

--- database/db.js
+++ database/db.js
@@ -3,8 +3,9 @@ import pg from 'pg';
 const pool = new pg.Pool({
     user: 'kipkemoi',
     password: '',
+    database: 'cat_facts_database',
     host: 'localhost',
-    port: 5433
+    port: 5432
 });
 
 export default pool;
--- routes/catfacts.js
+++ routes/catfacts.js
@@ -12,16 +12,51 @@ router.get(
     try {
       //try and retrieve data first from the db, if missing then fetch and save
       let allFacts = await pool.query('SELECT * FROM cat_facts');
-      if (!allFacts) {
+      if (!allFacts.rows) {
         const { data } = await handler.getListFromAPI();
+        // console.log(data);
         allFacts = data.forEach(async (fact) => {
           await pool.query(
-            'INSERT INTO cat_facts (id, userName, textDescription, updatedAt, animalType, createdAt) VALUES($1, $2, $3, $4, $5, $6) RETURNING * ',
-            [fact.id, fact.user, fact.text, fact.updatedAt, fact.type, fact.createdAt]);
+            'INSERT INTO cat_facts (userName, textDescription, updatedAt, animalType, createdAt) VALUES($1, $2, $3, $4, $5) RETURNING * ',
+            [fact.user, fact.text, fact.updatedAt, fact.type, fact.createdAt]);
         })
       }
-      console.log(allFacts);
-      res.send({ allFacts });
+      // console.log(allFacts.rows);
+      res.send(allFacts.rows);
+    } catch (err) {
+      res.send(err);
+      next(err);
+    }
+  }
+)
+
+router.put(
+  '/facts/:id',
+  async (req, res, next) => {
+    const { id } = req.params;
+    const { text } = req.body;
+    const currentTime = new Date().toISOString();
+    try {
+      const updateFact = await pool.query(
+        'UPDATE cat_facts SET textDescription = $1, updatedAt = $2 WHERE id = $3',
+        [text, currentTime, id]);
+      res.json('fact was successfully updated!');
+    } catch (err) {
+      res.send(err);
+      next(err);
+    }
+  }
+)
+
+router.delete(
+  '/facts/:id',
+  async (req, res, next) => {
+    const { id } = req.params;
+    try {
+      const updateFact = await pool.query(
+        'DELETE FROM cat_factS WHERE id = $1',
+        [id]);
+      res.json('fact was successfully deleted!');
     } catch (err) {
       res.send(err);
       next(err);
