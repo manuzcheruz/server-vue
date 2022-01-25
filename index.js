e9e659babc34d027bafb6a28b147f02e90194fc8
Kipkemoi Emmanuel <kipkemoi@Kipkemois-MacBook-Air.local>
2022-01-25 14:43:34 +0300

    added cors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import cors from 'cors';
 import config from './config.js';
 
 // routes 
@@ -13,6 +14,7 @@ const app = express();
 // Parse incoming requests data (https://github.com/expressjs/body-parser)
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
 
 // Require our routes into the application.
 app.use(routes);
